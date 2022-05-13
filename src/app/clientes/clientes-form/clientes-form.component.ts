a8be711074326786fb46f99aa990439d22082315
seu-usersaneme <kaiospinheiro@hotmail.com>
2022-05-13 01:20:41 -0300

    Commit do Front da aplicação de Clientes

---
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Cliente } from '../cliente'
+import { ClientesService } from '../../clientes.service'
+
+@Component({
+  selector: 'app-clientes-form',
+  templateUrl: './clientes-form.component.html',
+  styleUrls: ['./clientes-form.component.css']
+})
+export class ClientesFormComponent implements OnInit {
+
+  cliente: Cliente;
+  success: boolean = false;
+  errors: String[]=[];
+
+
+  constructor( private service: ClientesService ) {
+    this.cliente = new Cliente();
+
+   }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(){
+    this.service
+      .salvar(this.cliente)
+      .subscribe ({
+       error: (errorResponse) => this.errors = errorResponse.error.errors,
+       complete: () => console.info('complete') 
+   })
+ }
+
+}
